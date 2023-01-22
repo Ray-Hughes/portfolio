18a8199173759a0de2357bfbaca65d728a9788e1
Raymond Hughes <raymond.hughes@carepaths.com>
2023-01-22 13:44:57 -0500

    Adding new blog/about/resume pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'blog/index'
+  get 'about/index'
+  get 'resume/index'
+  root 'home#index'
 end
