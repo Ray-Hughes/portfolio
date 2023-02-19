970922ce88824035c930a57760a24a28c8e38140
Raymond Hughes <raymond.hughes@carepaths.com>
2023-02-19 16:21:58 -0500

    Adding styles

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,10 @@
 // Entry point for the build script in your package.json
+import "./main"
+import "aos"
+import "boxicons"
+import "typed"
+import "swiper"
+import "waypoints"
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
